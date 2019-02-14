4cbffa8722cca8e2d35e04a99bbcd4f245413689
Kristian Heruc <kristian@heruc.com>
2019-02-14 13:06:23 +0100

    Step 4: Styling the website

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,20 @@ const Header = () => (
   <div className="header-wrapper">
     <Logo />
     <Menu />
+    <style jsx>{`
+      .header-wrapper {
+        padding: 30px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      @media (max-width: 600px) {
+        .header-wrapper {
+          display: block;
+          text-align: center;
+        }
+      }
+    `}</style>
   </div>
 );
 
--- components/Logo.js
+++ components/Logo.js
@@ -3,8 +3,22 @@ import Link from 'next/link';
 const Logo = () => (
   <div className="logo">
     <Link href="/">
-      <a>Fi Studio</a>
+      <a>
+        <img src="/static/logo.png" />
+      </a>
     </Link>
+
+    <style jsx>{`
+      .logo img {
+        display: block;
+        width: 120px;
+      }
+      @media (max-width: 600px) {
+        .logo {
+          display: inline-block;
+        }
+      }
+    `}</style>
   </div>
 );
 
