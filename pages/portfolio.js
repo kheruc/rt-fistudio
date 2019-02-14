aa4cb5257f2f88b10b2fc98ea3ff2a8ffd95f487
Kristian Heruc <kristian@heruc.com>
2019-02-14 13:12:22 +0100

    Step 5: Adding content to pages

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,9 +1,20 @@
 import Layout from '../components/Layout';
+import Gallery from '../components/Gallery';
 
-const Portfolio = () => (
-  <Layout>
-    <h1>Portfolio</h1>
-  </Layout>
-);
+const Portfolio = () => {
+  const images = [];
+  const imageCount = 14;
+
+  for (let i = imageCount; i > 0; i--) {
+    images.push(`/static/portfolio/${i}.jpg`);
+  }
+
+  return (
+    <Layout>
+      <h1>Portfolio</h1>
+      <Gallery images={images} />
+    </Layout>
+  );
+};
 
 export default Portfolio;
