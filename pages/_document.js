e9479735a4d637270b7bf3d75ca7f35b3a7a4953
Kristian Heruc <kristian@heruc.com>
2019-09-06 12:32:02 +0200

    Remove unnecessary meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
         </Head>
         <body>
